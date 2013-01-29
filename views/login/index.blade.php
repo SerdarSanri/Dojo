7b166b6af5f002de3df163a5d8efb12a5e099b43
Rubem Mota <rubemmota89@gmail.com>
2013-01-29 16:47:56 +0000

    change to login page , so it will include
    change to login page , so it will include some html5 validations

--- views/login/index.blade.php
+++ views/login/index.blade.php
@@ -4,22 +4,44 @@
     <meta charset="utf-8">
     <title>Login</title>
     {{Asset::container('header')->styles()}}
-     <!--[if lt IE 9]>
-    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
+    <!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
 </head>
 <body>
-@if(Session::get('error'))
-    Sorry, your username or password was incorrect.
-@endif
 
-{{Form::open()}}
-{{Form::label('username', 'Username')}}
-{{Form::text('username')}}
-{{Form::label('password', 'Password')}}
-{{Form::password('password')}}
-{{Form::submit('Login', array('class' => 'btn btn-success'))}}
-{{Form::token()}}
-{{Form::close()}}
+    <div id="content-main">
+        <div class="row-fluid">
+            <div class="well span6 well-wrapper-form">
+
+                <form class="bs-docs-example form-inline" action="" method="post">
+                    {{Form::token()}}
+
+                    <fieldset>
+                        <legend>Dojo Administration</legend>
+                        @if(Session::get('error'))
+                        <p class="errornote alert alert-error fade in">
+                            <button data-dismiss="alert" class="close" type="button">×</button>
+                            Username/Password not found or incorrect!
+                        </p>
+                        @endif
+                        <div class="control-group">
+                            <input type="text" placeholder="Username" class="span4 focused" name="username" id="username" required></div>
+                        <div class="control-group">
+                            <input type="password" placeholder="Password" class="span4" name="password" id="password" required></div>
+                            <input class="btn btn-primary" type="submit" value="Login">
+
+                    </fieldset>
+                </form>
+            
+                <p class="forgotten-password">
+                    <a href="">Forgotten your password or username?</a>
+                </p>
+                <p class="link-site-name">
+                    <a href="">Dojo Bundle</a>
+                </p>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
